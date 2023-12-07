e1e980234405f1e69e5360a0046785cd9282a9c3
Adam-Arezza <arezza.adam@gmail.com>
2023-12-06 21:00:22 -0800

    rip day 3, add day 4

--- day_3.py
+++ day_3.py
@@ -11,31 +11,43 @@ test_engine = ["467..114..",
 
 with open('inputs/day_3.txt', 'r') as inputs:
     inputs = inputs.readlines()
+
+for i in range(len(inputs)):
+    inputs[i] = inputs[i].replace("\n", ".")
+
+dot_row = "".join(["." for i in inputs[0]])
+inputs.append(dot_row)
+inputs.insert(0, dot_row)
+
+for i in range(len(inputs)):
+    inputs[i] += "."
+    inputs[i] = "." + inputs[i]
 sum = 0
 part_numbers = []
-def search_for_symbol(arr, row_ind, start, end):
-    symbol = False
-    steps = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
-    for i in range(start, end + 1, 1):
-        print(f'substring index: {i}')
-        for s in steps:
-            try:
-                step = arr[row_ind + s[0]][i+ s[1]]
-                if row_ind + s[0] < 0:
-                    continue
-                if i + s[1] < 0:
-                    continue
-                print(f'position checked: {row_ind + s[0]},{i + s[1]}')
-                print(f'found: {step}')
-                if step != "." and not step.isdigit():
-                    symbol = True
-                    print("symbol found")
-                    break
-            except Exception as e:
-                print(e)
-    return symbol
+valid_in_row = [[] for i in range(len(inputs))]
 
-digit_ss = []                
+def search(arr, row, num):
+    symbol = False
+    start_ind = arr[row].find(num)
+    start_col = start_ind - 1
+    start_row = row - 1
+   
+    for r in range(3): 
+        for c in range(len(num) + 2):
+            try:
+                check_row = start_row + r
+                check_col = start_col + c
+                #if check_row < 0 or check_col < 0 or check_col > len(arr[0]) - 1 or check_row > len(arr) - 1:
+                #    continue
+                check_cell = arr[check_row][check_col]
+                if check_cell != "." and check_cell != "\n" and not check_cell.isdigit():
+                    symbol = True
+                    part_numbers.append(num)
+                    valid_in_row[row].append(num)
+            except:
+                print(check_row)
+                print(check_col)
+
 for i in range(len(inputs)):
     current_sub_str = ""
     for j in range(len(inputs[i])):
@@ -44,20 +56,22 @@ for i in range(len(inputs)):
         else:
             if len(current_sub_str) > 0:
                 start = inputs[i].find(current_sub_str,0)
-                sub_string_info = {"value":current_sub_str,
-                                   "row": i,
-                                   "range": [start, start + len(current_sub_str)-1]
-                               }
-                digit_ss.append(sub_string_info)
+                search(inputs, i, current_sub_str)
                 current_sub_str = ""
-                symbol_found = search_for_symbol(inputs, sub_string_info["row"], sub_string_info["range"][0], sub_string_info["range"][1])
-                if symbol_found:
-                    sum += int(sub_string_info["value"])
-                    part_numbers.append(sub_string_info["value"])
 
-for line in inputs:
-    print(line)
-#print(digit_ss)
+for n in part_numbers:
+    sum += int(n)
 print(part_numbers)
 print(sum)
-#guesses 536795,532672,
+
+for i in range(len(valid_in_row)):
+    print(valid_in_row[i])
+    print(inputs[i-1])
+    print(inputs[i])
+    print(inputs[i+1])
+    print("\n")
+
+#guesses 536795,532672,530799,
+#error rows
+# ....16....371........*.....-........*...........=..........202..........373.....6.749............28................*....675.....529............
+# 
