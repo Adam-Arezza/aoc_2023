5fdbc0557faa50ac0ab94b140150f18e9dacfac2
Adam Arezza <arezza.adam@gmail.com>
2023-12-13 06:28:44 -0800

    Day5 pt1

--- day_5.py
+++ day_5.py
@@ -1,31 +1,91 @@
-example = { "seeds": [79, 14, 55, 13],
-           "seed-to-soil map":
-           [[50, 98, 2],
-            [52, 50, 48]],
-           "soil-to-fertilizer map":
-           [[0, 15, 37],
-            [37, 52, 2],
-            [39, 0, 15]],
-           "fertilizer-to-water map":
-           [[49, 53, 8],
-            [0, 11, 42],
-            [42, 0, 7],
-            [57, 7, 4]],
-           "water-to-light map":
-           [[88, 18, 7],
-            [18, 25, 70]],
-           "light-to-temperature map":
-           [[45, 77, 23],
-            [81, 45, 19],
-            [68, 64, 13]],
-           "temperature-to-humidity map":
-           [[0, 69, 1],
-            [1, 0, 69]],
-           "humidity-to-location map":
-           [[60, 56, 37],
-            [56, 93, 4]]}
+# format of the maps
+#[destination range start, source range start, range length]
+# in seed-to-soil-map below
+# 98,99 map to 50,51
+# 50,...97 map to 52...99
+# therefore, seed 55 would map to soil 57
+# goal is to traverse through the map(s) to get to location#
+# then pick the lowest location number
+
+def parse_maps(maps):
+    seeds = []
+    current_map = ""
+    mapping = {}
+    for i in range(len(maps)):
+        map = maps[i].split("\n")
+        if map[-1] == "":
+            map.pop()
+        map = map[0].split()
+        if len(map) > 0 and not map[-1].isdigit():
+            map.pop()
+            current_map = map[0].replace("-","_")
+            if len(current_map) > 0:
+                mapping[current_map] = []
+        else:
+            if len(map) > 3:
+                for j in map:
+                    if j.isdigit():
+                        seeds.append(int(j))
+            else:
+                try:
+                    mapper = {}
+                    for i in range(len(map)):
+                        if i == 0:
+                            mapper["destination_start"] = int(map[i])
+                        if i == 1:
+                            mapper["source_start"] = int(map[i])
+                        if i == 2:
+                            mapper["range"] = int(map[i])
+                    if len(mapper.keys()) > 0:
+                        mapping[current_map].append(mapper)
+                except Exception as e:
+                    print(e)
+    return mapping,seeds
+
+
+def get_seed_location(seed,map):
+    src = seed
+    maps = []
+    for i in map:
+        #print(src)
+        #print(i)
+        for m in map[i]:
+            print(m)
+            if src >= m["source_start"] and src <= m["source_start"] + m["range"]-1:
+                #print(f'Found {src} in {range(m["source_start"],m["source_start"]+ m["range"]-1)}')
+                #print(f'Destination is {src} - {m["source_start"]} + {m["destination_start"]}')
+                src = m["destination_start"] + src - m["source_start"] 
+                maps.append(src)
+                break
+    print(f'Seed: {seed}, maps: {maps}')
+    return src
+
+
+with open("inputs/day_5_example.txt", "r") as example:
+    example = example.readlines()
 
 with open("inputs/day_5.txt","r") as inputs:
     inputs = inputs.readlines()
 
-print(inputs)
+mappings, seeds = parse_maps(example)
+print(seeds)
+print(mappings["soil_to_fertilizer"])
+
+final_locations = []
+for i in range(len(seeds)):
+    final_locations.append((seeds[i], get_seed_location(seeds[i],mappings)))
+print(final_locations)
+
+mappers, initial_seeds = parse_maps(inputs)
+seed_locations = []
+for i in range(len(initial_seeds)):
+    seed_locations.append((initial_seeds[i], get_seed_location(initial_seeds[i],mappers)))
+print(seed_locations)
+
+lowest_location_number = seed_locations[0][1]
+for seed,location in seed_locations:
+    if location < lowest_location_number:
+        lowest_location_number = location
+    print(f'initial seed: {seed} location: {location}')
+print(lowest_location_number)
+
